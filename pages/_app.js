cf0354578aecd55503dba7b40a52dc1229642f3b
Jonathan Värild <Jonathan.varild@gmail.com>
2022-12-04 11:57:12 +0100

    Project start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
