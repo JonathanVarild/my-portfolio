5cd6c076ce7bc2398df9818ed489cbbebda18a22
Jonathan Värild <Jonathan.varild@gmail.com>
2022-12-04 12:46:27 +0100

    Add FontAwesome Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import "../styles/globals.css";
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
