9054ae98b7704be4bb79446b1021f7b92f235759
Jonathan Värild <Jonathan.varild@gmail.com>
2022-12-11 18:24:14 +0100

    Additions to Start Section

--- components/StartSection.js
+++ components/StartSection.js
@@ -1,11 +1,21 @@
 import styles from "../styles/startsection.module.css";
+import Image from "next/image";
+import memoji_happy from "../public/memoji_happy_hq.png";
 
 function StartSection() {
 	return (
 		<>
 			<div className={styles.sectionBackground} />
-			<section id={styles.startSection}></section>
-            
+			<section id={styles.startSection}>
+				<div className={styles.container}>
+					<h1>Jonathan Värild</h1>
+					<h2>
+						presenting <span>my portfoilio.</span>
+					</h2>
+					<h3>Full-stack developer and designer, focused on creating exceptional digital experiences.</h3>
+				</div>
+				<Image src={memoji_happy} height={"1rem"}/>
+			</section>
 		</>
 	);
 }
