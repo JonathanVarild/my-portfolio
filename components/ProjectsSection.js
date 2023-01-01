5614cc2ce874f629aae4eb15865bd920c15dfc8f
Jonathan Värild <Jonathan.varild@gmail.com>
2023-01-01 23:11:48 +0100

    Added ProjectSection

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -1,10 +1,20 @@
 import sectionStyles from "../styles/section.module.css";
+import styles from "../styles/projectssection.module.css";
+import ProjectWidget from "./ProjectWidget";
 
 function ProjectsSection() {
 	return (
 		<section id="projects">
 			<h2>My Projects</h2>
-            <div className={sectionStyles.content}>These are some of the projects that I've worked on and feel extra proud about.</div>
+			<div className={sectionStyles.content}>These are some of the projects that I've worked on and feel extra proud about.</div>
+			<div className={styles.projectsContainer}>
+				<ProjectWidget title="Test" subtitle="Server Plugin" tags={["Lua", "Garry's Mod", "UI", "For Sale", "test"]} />
+				<ProjectWidget title="Test" subtitle="Server Plugin" tags={["Lua", "Garry's Mod", "UI", "For Sale"]} />
+				<ProjectWidget title="Test" subtitle="Server Plugin" tags={["Lua", "Garry's Mod", "UI", "For Sale"]} />
+				<ProjectWidget title="Test" subtitle="Server Plugin" tags={["Lua", "Garry's Mod", "UI", "For Sale"]} />
+				<ProjectWidget title="Test" subtitle="Server Plugin" tags={["Lua", "Garry's Mod", "UI", "For Sale"]} />
+				<ProjectWidget title="Test" subtitle="Server Plugin" tags={["Lua", "Garry's Mod", "UI", "For Sale"]} />
+			</div>
 		</section>
 	);
 }
