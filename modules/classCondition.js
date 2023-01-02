bcd4152ccbccc32684fd7796a697e7ab99e0c0cc
Jonathan Värild <Jonathan.varild@gmail.com>
2023-01-02 23:27:09 +0100

    Small change to ClassCondition module

--- modules/classCondition.js
+++ modules/classCondition.js
@@ -3,7 +3,7 @@ function classCondition(...args) {
 
 	args.forEach((element) => {
 		if (!Array.isArray(element)) toReturn.push(element);
-		else if (element[1] === true || element[1] == undefined) toReturn.push(element[0]);
+		else if (element[1] === true) toReturn.push(element[0]);
 	});
 
 	return toReturn.join(" ");
